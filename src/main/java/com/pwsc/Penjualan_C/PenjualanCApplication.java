b2f402468393a9cbb84f6af69a3faa509377652f
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-10-17 22:44:17 +0700

    my first commit

---
+++ src/main/java/com/pwsc/Penjualan_C/PenjualanCApplication.java
@@ -0,0 +1,13 @@
+package com.pwsc.Penjualan_C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenjualanCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenjualanCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwsc/Penjualan_C/PenjualanCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwsc.Penjualan_C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenjualanCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
